5121d25bddcfd8af13690aa31b2e5bc69db7e86f
Si Hui Ong <s10187031@connect.np.edu.sg>
2023-06-14 17:10:26 +0800

    added user data handling

--- CSC1107_22_user.c
+++ CSC1107_22_user.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // memory allocation functions
 #include <time.h>
+#include <errno.h> //error handling 
+#include <fcntl.h> // file control
+#include <string.h> 
+#include <ctype.h> // character handling
+#include <unistd.h> // file descriptor operations
+#include <stdint.h> // fixed-width integer types
+#define BUF_SIZE 1024 // size of the plaintext buffer 
+#define DEVICE_PATH "/dev/my_device"
 
 // func prototype to be moved in header file
 typedef struct DateTime dt; // give struct alias dt
@@ -56,18 +64,103 @@ static dt generate_datetime()
 
 void format_sentence(char *sentence, dt dt)
 {
-    sprintf(sentence, "It is %s on %s now.\n", dt.time, dt.date);
+    sprintf(sentence, "It is %s on %s now.", dt.time, dt.date);
 }
 
-// Secure Hashing Algorithm 512 (SHA-512), SHA-384, SHA-256, SHA-1, or MD5
-//
-//
+// Secure Hashing Algorithm MD5, 512 (SHA-512), SHA-384, SHA-256 or SHA-1
+// hash types
+typedef enum hash {
+    MD5 = 0,
+    SHA512,
+    SHA384,
+    SHA256,
+    SHA1
+} hash_t;
 
-int main()
+// structure to hold sentence from user space and the hashing algorithm chosen by user
+typedef struct usespace {
+    uint8_t plaintext[BUF_SIZE]; // array of unsigned 8-bit integer with the pre-defined buffer size
+    hash_t algo;
+} userspace_t;
+
+int main(void)
 {
+    // dev: file descriptor of the device file
+    // ret: return value
+    // digest: array of characters to store computed hash digest
+    int dev, ret;
+    char digest[BUF_SIZE];
+
+    // open the character device
+    dev = open(DEVICE_PATH, O_RDWR); // open device in read write mode
+    if (dev < 0){
+        perror("Failed to open device"); // error handling
+        return errno;
+    }
+
     char sentence[100];
     dt dt = generate_datetime();
-    format_sentence(sentence, dt);
+    format_sentence(sentence, dt); // sentence for datetime
 
-    printf("%s", sentence);
-}
+    // get user input for hashing algorithm
+    int choice;
+    printf("Select a Hashing Algorithm (1-5) from below:\n1. MD5\n2. SHA-512\n3. SHA-382\n4. SHA-256\n5. SHA-1\n\nChoice: ", stdout);
+    scanf("%d", &choice);
+    if (0 < choice || choice > 5 ) {
+        puts("Please select an option from 1 to 5.");
+        return -1;
+    }
+
+    userspace_t us; 
+    strncpy((char*)us.plaintext, sentence, BUF_SIZE); // convert characters of the sentence to bytes and store in user.plaintext array to access data byte by byte if needed 
+    // provide hashing algo based on user input
+    if (choice == 1){
+        us.algo = MD5;
+    } else if (choice == 2){
+        us.algo = SHA512;
+    } else if (choice == 3){
+        us.algo = SHA384;
+    } else if (choice == 4){
+        us.algo = SHA256;
+    } else if (choice == 5){
+        us.algo = SHA1;
+    }
+
+    // write to device
+    ret = write(dev, &us, sizeof(userspace_t));
+    if (ret < 0){
+        perror("Error while sending data to kernel space");
+        return errno;
+    }
+
+    // read from device
+    ret = read(dev, digest, BUF_SIZE);
+    if (ret < 0){
+        perror("Error while reading data to kernel space");
+        return errno;
+    }
+
+    // calculate number of bytes to print in hash digest based on the algo
+    // MD5 hash digest = 128 bits = 16 bytes
+    // SHA-512 hash digest = 512 bits = 64 bytes
+    // SHA-384 hash digest = 384 bits = 48 bytes
+    // SHA-256 hash digest = 256 bits = 32 bytes
+    // SHA-1 hash digest = 160 bits = 20 bytes
+    size_t bytes_to_print = (us.algo == MD5) ? 16 :
+                            (us.algo == SHA512) ? 64 :
+                            (us.algo == SHA384) ? 48 :
+                            (us.algo == SHA256) ? 32 :
+                            (us.algo == SHA1) ? 20 :
+                            (puts("Algorithm not supported"), -1);
+    
+    // hashing algorithms to be implemented
+
+    printf("Original Sentence: %s \nHashed Sentence: \"", us.plaintext);
+    for (size_t i=0; i<bytes_to_print; i++)
+        printf("%02x", (unsigned char)digest[i]);
+    puts("\"");
+    printf("\nHashing Algorithm: %s", us.algo);
+
+    return 0;
+}   
+
