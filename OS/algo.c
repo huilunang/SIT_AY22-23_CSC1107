f1d0d2ad7b299e255fbb4a5243002686e29ff757
keaganwongkl <44427454+keaganwongkl@users.noreply.github.com>
2023-07-03 14:32:03 +0800

    Update algo.c
    FCFS able to be the fastest in 1 scenario

--- OS/algo.c
+++ OS/algo.c
@@ -7,11 +7,11 @@
 
 #include <stdio.h>
 #include "sort.c"
-#include <time.h>
 
 main()
 
 {
+
     int option = 0;
     int choice;
     int loop = 0;
@@ -32,107 +32,137 @@ main()
 
     while (option != 1)
     {
+        float waitArray[5];
+        float turnArray[5];
+        float resArray[5];
 
         printf("1)FCFS \n2)SJF \n3)SRTF \n4)RR \n5)Priority Scheduling: \n6)Exit Program \n");
         scanf("%d", &choice);
-        FILE *file = fopen("C:\\Users\\Keag\\Desktop\\SITstoofs\\Y1S3\\OS\\Q2_Group_Number.txt", "w");
 
-        if (file == NULL)
-        {
-            printf("Failed to open the file.\n");
-            return 1;
-        }
+        int loop = 0;
         switch (choice)
         {
         case 1: // FCFS being the shortest
-            //randomise the values
-            randomProcess(processes);
-            for(int i =0;i<6;i++){
-                processes2[i]=processes[i];
+            // randomise the values
+            while (loop == 0)
+            {
+                FILE *file = fopen("C:\\Users\\Keag\\Desktop\\SITstoofs\\Y1S3\\OS\\Q2_Group_Number.txt", "w");
+
+                if (file == NULL)
+                {
+                    printf("Failed to open the file.\n");
+                    return 1;
+                }
+                randomProcess(processes);
+
+                for (int i = 0; i < 6; i++)
+
+                {
+                    processes2[i] = processes[i];
+                }
+                fcfsSort(processes);
+                calculateTimes(processes);
+                process(processes);
+                turnArray[0] = writeFileTurn(processes, file, "FCFS");
+                waitArray[0] = writeFileWait(processes, file, "FCFS");
+                resArray[0] = writeFileRes(processes, file, "FCFS");
+
+                // reset back to original
+                reset(processes, processes2);
+
+                // SJF start here
+                fcfsSort(processes);
+                burstTimeSort(processes, smallTime, temp2);
+                sjfSort(processes, currentTime, temp2);
+                calculateTimes(processes);
+                process(processes);
+                turnArray[1] = writeFileTurn(processes, file, "SJF");
+                waitArray[1] = writeFileWait(processes, file, "SJF");
+                resArray[1] = writeFileRes(processes, file, "SJF");
+
+                // reset back to original
+                reset(processes, processes2);
+
+                // SRTF start here
+                srtfSort(processes);
+                calculateTimes(processes);
+                process(processes);
+                turnArray[2] = writeFileTurn(processes, file, "SRTF");
+                waitArray[2] = writeFileWait(processes, file, "SRTF");
+                resArray[2] = writeFileRes(processes, file, "SRTF");
+
+                // reset back to original
+                reset(processes, processes2);
+                // RR start here
+                randomProcess(processes);
+                rrSort(processes);
+                calculateTimes(processes);
+                process(processes);
+                turnArray[3] = writeFileTurn(processes, file, "RR");
+                waitArray[3] = writeFileWait(processes, file, "RR");
+                resArray[3] = writeFileRes(processes, file, "RR");
+
+                // PS start here
+                prioSort(processes);
+                calculateTimes(processes);
+                process(processes);
+                turnArray[4] = writeFileTurn(processes, file, "PS");
+                waitArray[4] = writeFileWait(processes, file, "PS");
+                resArray[4] = writeFileRes(processes, file, "PS");
+
+                // turnArray[n] = floatTurnTime;
+                // waitArray[n] = floatWaitTime;
+                // resArray[n] = floatResTime;
+
+                float fastestTurn = findFast(turnArray, 5);
+                float fastestWait = findFast(waitArray, 5);
+                float fastestRes = findFast(resArray, 5);
+
+                printf("%f\n", fastestTurn);
+                printf("%f\n", fastestWait);
+                printf("%f\n", fastestRes);
+                printf("%f\n", turnArray[0]);
+                printf("%f\n", waitArray[0]);
+                printf("%f\n", resArray[0]);
+                fclose(file);
+                if (fastestTurn == turnArray[choice - 1] || fastestWait == waitArray[choice - 1] || fastestRes == resArray[choice - 1])
+                {
+                    loop = 1;
+                }
             }
-            //process(processes);
-            //process(processes2);
-
-            fcfsSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "FCFS");
-            
-            //reset back to original
-            reset(processes,processes2);
-
-            //SJF start here
-            fcfsSort(processes);
-            burstTimeSort(processes, smallTime, temp2);
-            sjfSort(processes, currentTime, temp2);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "SJF");
-
-            //reset back to original
-            reset(processes,processes2);
-
-            //SRTF start here
-            srtfSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "SRTF");
-            
-            //reset back to original
-            reset(processes,processes2);
-            //RR start here
-            randomProcess(processes);
-            rrSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "RR");
-
-            //PS start here
-            prioSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "PS");
-
-            fclose(file);
-
             break;
+
         case 2: // SJF being the shortest
-
-            randomProcess(processes);
-            // Sort the AT
-            fcfsSort(processes);
-            burstTimeSort(processes, smallTime, temp2);
-            // Sorting by SJF
-            sjfSort(processes, currentTime, temp2);
-            calculateTimes(processes);
-            // After sorting
-            process(processes);
-            writeFile(processes, file, "SJF");
-            break;
-
-        case 3: // srtf
-            randomProcess(processes);
-            srtfSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "SRTF");
-            break;
-        case 4: // RR
-            randomProcess(processes);
-            rrSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "RR");
-
-            break;
-        case 5:
-            //PS
-            randomProcess(processes);
-            prioSort(processes);
-            calculateTimes(processes);
-            process(processes);
-            writeFile(processes, file, "PS");
-            break;
+                /*
+                            while (loop == 0)
+                            {
+                                doAlgo();
+                                loop = 1;
+                            }
+                            break;
+    
+                        case 3: // srtf
+                            while (loop == 0)
+                            {
+                                doAlgo();
+                                loop = 1;
+                            }
+                            break;
+                        case 4: // RR
+                            while (loop == 0)
+                            {
+                                doAlgo();
+                                loop = 1;
+                            }
+                            break;
+                        case 5: // PS
+                            while (loop == 0)
+                            {
+                                doAlgo();
+                                loop = 1;
+                            }
+                            break;
+                */
         case 6:
             return 0;
         default:
