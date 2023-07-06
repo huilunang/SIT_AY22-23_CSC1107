4377565801eaf25160f3d43b0bc6c01825e2024e
huilunang <huilun2000@gmail.com>
2023-07-06 12:23:06 +0800

    Add comments and cleanup code

--- Q1/CSC1107_22_kernel.c
+++ Q1/CSC1107_22_kernel.c
@@ -15,7 +15,7 @@ MODULE_DESCRIPTION("CSC1107_22 Project Module");
 
 #define DEV_NAME "CSC1107_22"        // Name of the character device
 #define DEV_CLASS "CSC1107_22_CLASS" // Name of the device class
-#define BUF_SIZE 1024                // Size of plaintext buffer
+#define BUF_SIZE 1024                // size of the text buffer
 
 // Functions prototypes
 static int device_open(struct inode *, struct file *);
@@ -23,31 +23,23 @@ static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-// global definitions
-static int dev_num;                        // Major number of the character device
-static struct class *csc1107_class = NULL; // Device driver class pointer
-static struct device *csc1107_dev = NULL;  // Device driver pointer
-static size_t open_count = 0;              // Number of types this device has been opened
-static char digest[BUF_SIZE];              // Result of hashing process
-
-// Userspace struct
+// supported has types
 typedef enum hash
 {
     MD5 = 0,
-    SHA1,
-    SHA256,
     SHA512,
-    SHA384
+    SHA384,
+    SHA256,
+    SHA1
 } hash_t;
 
+// userspace struct
 typedef struct userspace
 {
     u8 plaintext[BUF_SIZE];
     hash_t algorithm;
-    u8 hashtext[BUF_SIZE];
+    uint8_t hashtext[BUF_SIZE];
 } userspace_t;
-
-static userspace_t userspace;              // Data from userspace
 
 // File operation struct
 // Map local functions to default system calls
@@ -59,8 +51,17 @@ static struct file_operations fo = {
     .write = device_write,
 };
 
-// Entry point functions(__init and __exit)
-// Initalising a character device
+// global definitions
+static int dev_num;                        // Major number of the character device
+static struct class *csc1107_class = NULL; // Device driver class pointer
+static struct device *csc1107_dev = NULL;  // Device driver pointer
+static size_t open_count = 0;              // Number of types this device has been opened
+static char digest[BUF_SIZE];              // Result of hashing process
+static userspace_t userspace;              // Data from userspace
+static size_t bytes_not_copied = 0;        // Error check
+static size_t bytes_to_copy = 0;           // Size of digest
+
+/* Initalising a character device */
 static int __init device_init(void)
 {
     pr_info("%s: Loading, please wait...\n", DEV_NAME);
@@ -100,6 +101,7 @@ static int __init device_init(void)
     return 0;
 }
 
+/* Cleanup resources of character device */
 static void __exit device_exit(void)
 {
     device_destroy(csc1107_class, MKDEV(dev_num, 0)); // Remove device
@@ -107,13 +109,13 @@ static void __exit device_exit(void)
     class_destroy(csc1107_class);                     // Destroy the class
     unregister_chrdev(dev_num, DEV_NAME);             // Unregister device major number
 
-    pr_info("%s: module successfully unloaded\n", DEV_NAME);
+    pr_info("%s: Module successfully unloaded\n", DEV_NAME);
 }
 
 /* This function is invoked each time an userspace process tries
  * to open the character device with 'open()' syscall. It is usually
  * being used to set up the environment for the rest of the module but
- * since we do not have anything to configure, we will just
+ * since we do not have anything to configure, it will just
  * print out a message */
 static int device_open(struct inode *inodep, struct file *filep)
 {
@@ -122,49 +124,28 @@ static int device_open(struct inode *inodep, struct file *filep)
     return 0;
 }
 
-/* This function is invoked each time an userspace process
- * closes the character device. It is usually being used
+/* This function is invoked each time an userspace process closes the
+ * character device with 'close()' syscall. It is usually being used
  * to free allocated resources but since we have not anything
- * to do, we will just print out a message */
+ * to do, it will just print out a message */
 static int device_release(struct inode *inodep, struct file *filep)
 {
-    pr_info("%s: device closed successfully\n", DEV_NAME);
+    pr_info("%s: Device closed successfully\n", DEV_NAME);
 
     return 0;
 }
 
-
-/* Read from kernel
-    - invoked upon "Enter"/"Space" key
-
-    return:
-        char* sentence, user_hash, hash_type, kernel_hash, compare
-*/
-// void device_read() {}
-
-/* This function is invoked each time we call the 'read()' syscall
- * from userspace. In our module, we will use the function
- * 'copy_to_user' to send back to the user the result(i.e., the hashed string) */
+/* This function is invoked each time the 'read()' syscall is called
+ * from userspace. In this module, it will use the function 'copy_to_user'
+ * to send back to the user the result (i.e., the hashed string) */
 static ssize_t device_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
 {
-    size_t bytes_not_copied = 0;
-    // calculate number of bytes to copy in hash digest based on the algo
-    // SHA-512 hash digest = 512 bits = 64 bytes
-    // SHA-384 hash digest = 384 bits = 48 bytes
-    // SHA-256 hash digest = 256 bits = 32 bytes
-    // SHA-1 hash digest = 160 bits = 20 bytes
-    // MD5 hash digest = 128 bits = 16 bytes
-    size_t bytes_to_copy = (userspace.algorithm == SHA512) ? 64 :
-                            (userspace.algorithm == SHA384) ? 48 :
-                            (userspace.algorithm == SHA256) ? 32 :
-                            (userspace.algorithm == SHA1) ? 20 :
-                            16;
-
-    // copy_to_user returns 0 on success while it returns
-    // the number of bytes not copied on errors
+    // 'copy_from_user' pass data to userspace
+    //  it returns 0 on success and number of bytes not copied on error
     bytes_not_copied = copy_to_user(buffer, digest, bytes_to_copy);
+    // check errors
     if (bytes_not_copied)
-    { // Check errors
+    {
         pr_warn("%s: Failed to send result to userspace\n", DEV_NAME);
         return -EFAULT;
     }
@@ -172,90 +153,79 @@ static ssize_t device_read(struct file *filep, char *buffer, size_t len, loff_t 
     return 0;
 }
 
-
-/* Write into kernel
-    - print user space payloads and generated hash in kernel space
-    - print result of comparing user space hash is same as kernel space hash
-
-    param:
-        char* sentence, user_hash, hash_type
-    return:
-        None
-*/
-// void device_write() {}
-
-/* This function is invoked each time we call the 'write()' syscall
- * from userspace. In our module, we will use the function 'write_to_user'
- * to retrieve user data. We will then hash the plaintext string according to
- * the chosen algorithm using Linux cryptographic APIs */
+/* This function is invoked each time the 'write()' syscall is called
+ * from userspace. In this module, it will use the function 'copy_from_user'
+ * to retrieve data from userspace. It will then hash the plaintext string
+ * according to the chosen algorithm using Linux cryptographic APIs */
 static ssize_t device_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
 {
-    size_t bytes_not_copied = 0;
+    int err;
+    char *algo_str;
     struct crypto_shash *algorithm;
     struct shash_desc *desc;
-    int err;
-    size_t bytes_to_copy;
-    char* algoStr;
 
-    // 'copy_from_user' method returns 0 on success and the number of bytes not copied
-    // on error
+    // 'copy_from_user' gets data from userspace
+    //  it returns 0 on success and number of bytes not copied on error
     bytes_not_copied = copy_from_user(&userspace, buffer, sizeof(userspace_t));
-    if (bytes_not_copied)
-    { // Check errors
-        pr_warn("%s: error while copying %zu bytes from userspace\n", DEV_NAME, bytes_not_copied);
+    // check errors
+    if (bytes_not_copied != 0)
+    {
+        pr_warn("%s: Error while copying %zu bytes from userspace\n", DEV_NAME, bytes_not_copied);
         return -EFAULT;
     }
 
-    // Select hashing algorithm
+    // select hashing algorithm
     switch (userspace.algorithm)
     {
-    case MD5:
-        algorithm = crypto_alloc_shash("md5", 0, 0);
-        bytes_to_copy = 16;
-        algoStr = "MD5";
-        break;
-    case SHA512:
-        algorithm = crypto_alloc_shash("sha512", 0, 0);
-        bytes_to_copy = 64;
-        algoStr = "SHA512";
-        break;
-    case SHA384:
-        algorithm = crypto_alloc_shash("sha384", 0, 0);
-        bytes_to_copy = 48;
-        algoStr = "SHA384";
-        break;
-    case SHA256:
-        algorithm = crypto_alloc_shash("sha256", 0, 0);
-        bytes_to_copy = 32;
-        algoStr = "SHA256";
-        break;
-    case SHA1:
-        algorithm = crypto_alloc_shash("sha1", 0, 0);
-        bytes_to_copy = 20;
-        algoStr = "SHA1";
-        break;
-
-    default:
-        pr_alert("%s: hashing algorithm not recognized\n", DEV_NAME);
-        return -EFAULT;
+        case MD5:
+            algorithm = crypto_alloc_shash("md5", 0, 0);
+            bytes_to_copy = 16; // MD5 hash digest = 128 bits = 16 bytes
+            algo_str = "MD5";
+            break;
+        case SHA512:
+            algorithm = crypto_alloc_shash("sha512", 0, 0);
+            bytes_to_copy = 64; // SHA-512 hash digest = 512 bits = 64 bytes
+            algo_str = "SHA512";
+            break;
+        case SHA384:
+            algorithm = crypto_alloc_shash("sha384", 0, 0);
+            bytes_to_copy = 48; // SHA-384 hash digest = 384 bits = 48 bytes
+            algo_str = "SHA384";
+            break;
+        case SHA256:
+            algorithm = crypto_alloc_shash("sha256", 0, 0);
+            bytes_to_copy = 32; // SHA-256 hash digest = 256 bits = 32 bytes
+            algo_str = "SHA256";
+            break;
+        case SHA1:
+            algorithm = crypto_alloc_shash("sha1", 0, 0);
+            bytes_to_copy = 20; // SHA-1 hash digest = 160 bits = 20 bytes
+            algo_str = "SHA1";
+            break;
+        default:
+            pr_alert("%s: Hashing algorithm not recognized\n", DEV_NAME);
+            return -EFAULT;
     }
 
-    // Check if selected algorithm is available in the system
+    // check if selected algorithm is available in the system
     if (IS_ERR(algorithm))
-    { // Check errors
+    {
         pr_alert("%s: Hashing algorithm not supported\n", DEV_NAME);
         return -EFAULT;
     }
 
+    // allocate memory for descriptor/context which holds
+    // information on cryptographic operations
     desc = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(algorithm), GFP_KERNEL);
+    // check errors
     if (!desc)
-    { // check errors
+    {
         pr_err("%s: failed to allocate memory(kmalloc)\n", DEV_NAME);
         return -ENOMEM;
     }
     desc->tfm = algorithm;
 
-    // Initialize shash API
+    // initialize shash API
     err = crypto_shash_init(desc);
     if (err)
     {
@@ -263,7 +233,7 @@ static ssize_t device_write(struct file *filep, const char *buffer, size_t len, 
         goto out;
     }
 
-    // Execute hash function
+    // execute hash function
     err = crypto_shash_update(desc, userspace.plaintext, strlen(userspace.plaintext));
     if (err)
     {
@@ -271,7 +241,7 @@ static ssize_t device_write(struct file *filep, const char *buffer, size_t len, 
         goto out;
     }
 
-    // Write the result to a new char buffer
+    // write the result to new char buffer (digest)
     err = crypto_shash_final(desc, digest);
     if (err)
     {
@@ -279,31 +249,29 @@ static ssize_t device_write(struct file *filep, const char *buffer, size_t len, 
         goto out;
     }
 
-    // // received hashed_sentence from kernel
-    // for (size_t i = 0; i < bytes_to_copy; i++)
-    //     snprintf(&digest_hex[2 * i], 3, "%02x", (unsigned char)digest[i]); // convert digest bytes to hexadecimal
-    
-    // Finally, clean up resources
+    // finally, clean up resources
     crypto_free_shash(algorithm);
     kfree(desc);
 
-    pr_info("%s: String successfully hashed. Read from this device to get the result\n", DEV_NAME);
-    pr_info("Original Sentence: %s", userspace.plaintext);
-    pr_info("Hashed Sentence from user space: %s", userspace.hashtext);
-    pr_info("Hashing Algorithm: %s", algoStr);
-    pr_info("Hashed Sentence from kernel space: ");
+    // output results
+    pr_info("%s: String successfully hashed. Read from this device to get the result\n\n", DEV_NAME);
+    pr_info("%s: Original Sentence                 : %s", DEV_NAME, userspace.plaintext);
+    pr_info("%s: Hashed Sentence from user space   : %s", DEV_NAME, userspace.hashtext);
+    pr_info("%s: Hashing Algorithm                 : %s", DEV_NAME, algo_str);
+    pr_info("%s: Hashed Sentence from kernel space : ", DEV_NAME);
     for (size_t i = 0; i < bytes_to_copy; i++)
         pr_cont("%02x", digest[i]);
-    pr_cont("\n");
+    pr_cont("\n\n");
 
     return 0;
 
-out: // Manage errors
+// manage errors
+out:
     crypto_free_shash(algorithm);
     kfree(desc);
     return err;
 }
 
-// Register __init and __exit functions
+/* Register __init and __exit functions */
 module_init(device_init);
 module_exit(device_exit);
--- Q1/CSC1107_22_user.c
+++ Q1/CSC1107_22_user.c
@@ -1,76 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h> // memory allocation functions
-#include <time.h>
-#include <errno.h> //error handling
-#include <fcntl.h> // file control
 #include <string.h>
-#include <ctype.h>  // character handling
-#include <unistd.h> // file descriptor operations
-#include <stdint.h> // fixed-width integer types
-#include <openssl/evp.h>
+#include <stdlib.h>      // memory allocation functions
+#include <time.h>        // datetime
+#include <errno.h>       // error handling
+#include <fcntl.h>       // file control
+#include <ctype.h>       // character handling
+#include <unistd.h>      // file descriptor operations
+#include <stdint.h>      // fixed-width integer types
+#include <openssl/evp.h> // hashing function
 
-#define BUF_SIZE 1024 // size of the plaintext buffer
+#define BUF_SIZE 1024    // size of the text buffer
 #define DEVICE_PATH "/dev/CSC1107_22"
 
-// func prototype to be moved in header file
+// function prototypes
 typedef struct DateTime dt; // give struct alias dt
 static struct DateTime generate_datetime();
-void format_sentence(char sentence[], dt dt);
+void format_sentence(char *sentence, dt dt);
 
-typedef struct DateTime
-{
-    char date[40], time[40];
-} dt;
-
-// get date and time
-static dt generate_datetime()
-{
-    dt dt;
-    // vars to store datetime components
-    int hours, minutes, seconds, day, year;
-    char *month;
-
-    // variable to store current time
-    time_t now;
-    // & means gets the address of where now variable exists
-    // return time will be stored in that address of now
-    time(&now);
-
-    // localtime converts a `time_t` value to calendar time and
-    // returns a pointer to a `tm` structure with its members
-    // filled with the corresponding values
-    struct tm *local = localtime(&now);
-
-    // // hours in 24-hr format
-    // hours = local->tm_hour;
-    // // minutes 0 - 59
-    // minutes = local->tm_min;
-    // // seconds 0 - 59
-    // seconds = local->tm_sec;
-    // // day 1 - 31
-    // day = local->tm_mday;
-    // // month 0 - 11
-    // strftime(month, 20, "\%B", local);
-    // // year since 1900
-    // year = local->tm_year + 1900;
-
-    // // set datetime
-    // sprintf(dt.date, "%dth %s %d", day, month, year);
-    // sprintf(dt.time, "%d:%d:%d", hours, minutes, seconds);
-
-    strftime(dt.date, 40, "%dth %B %Y", local);
-    strftime(dt.time, 40, "%H:%M:%S", local);
-
-    return dt;
-}
-
-void format_sentence(char *sentence, dt dt)
-{
-    sprintf(sentence, "It is %s on %s now.", dt.time, dt.date);
-}
-
-// Secure Hashing Algorithm MD5, 512 (SHA-512), SHA-384, SHA-256 or SHA-1
-// hash types
+// supported has types
 typedef enum hash
 {
     MD5 = 0,
@@ -80,102 +27,159 @@ typedef enum hash
     SHA1
 } hash_t;
 
-// structure to hold sentence from user space and the hashing algorithm chosen by user
+// store date and time
+typedef struct DateTime
+{
+    char date[40], time[40];
+} dt;
+
+// store sentence, hashing algorithm and generated hash from user space
 typedef struct userspace
 {
-    uint8_t plaintext[BUF_SIZE]; // array of unsigned 8-bit integer with the pre-defined buffer size
+    uint8_t plaintext[BUF_SIZE]; // array of unsigned 8-bit integer
     hash_t algo;
     uint8_t hashtext[BUF_SIZE];
 } userspace_t;
 
-// hashing functions using the OpenSSL library - EVP, a high-level cryptographic interface that provides a unified API for various cryptographic operations including hash functions
-void hash(const char *sentence, char *hashed_sentence, const EVP_MD *hash_algo)
-{                                                                    // EVP_MD structure represents a digest algo (cryptographic hash function)
-    EVP_MD_CTX *ctx = EVP_MD_CTX_new();                              // this structure is used for the context of a hash computation, maintains the state
-    EVP_DigestInit_ex(ctx, hash_algo, NULL);                         // initialize the hash context for a specific algo
-    EVP_DigestUpdate(ctx, sentence, strlen(sentence));               // update the hash computation with input data -> sentence
-    EVP_DigestFinal_ex(ctx, (unsigned char *)hashed_sentence, NULL); // finalizes the hash computation and obtain resulting hash value
+
+// get date and time
+static dt generate_datetime()
+{
+    dt dt;
+    time_t now; // variable to store current time
+    time(&now); // store time in address of now
+
+    // localtime converts a `time_t` value to calendar time and
+    // returns a pointer to a `tm` structure with its members
+    // filled with the corresponding values
+    struct tm *local = localtime(&now);
+    strftime(dt.date, 40, "%dth %B %Y", local);
+    strftime(dt.time, 40, "%H:%M:%S", local);
+
+    return dt;
+}
+
+// format sentence with date and time
+void format_sentence(char *sentence, dt dt)
+{
+    sprintf(sentence, "It is %s on %s now.", dt.time, dt.date);
+}
+
+// hashing functions using the OpenSSL library - EVP
+// EVP_MD structure represents a digest algo (cryptographic hash function)
+// this structure is used for the context of a hash computation,
+// maintains the state initialize the hash context for a specific algo
+// update the hash computation with input data -> sentence
+// finalizes the hash computation and obtain resulting hash value
+void hash(const char *sentence, uint8_t *hashed_sentence, const EVP_MD *hash_algo)
+{
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, hash_algo, NULL);
+    EVP_DigestUpdate(ctx, sentence, strlen(sentence));
+    EVP_DigestFinal_ex(ctx, (unsigned char *)hashed_sentence, NULL);
     EVP_MD_CTX_free(ctx);
 }
 
 int main(void)
 {
-    // dev: file descriptor of the device file
-    // ret: return value
-    // digest: array of characters to store computed hash digest
-    int dev, ret;
-    char digest[BUF_SIZE];
 
-    // open the character device
-    dev = open(DEVICE_PATH, O_RDWR); // open device in read write mode
-    if (dev < 0)
+    int dev;                 // file descriptor of the device file
+    int ret;                 // return value
+    char digest[BUF_SIZE];   // to store computed hash digest
+    userspace_t us;          // to store required fields and pass to kernel space
+
+    char sentence[100];      // to store generated sentence
+    dt dt;                   // to store generated system datetime
+
+    int choice;              // selection of hashing algorithm
+    char *algo_str;          // selected type of hashing algorithm
+    const EVP_MD *hash_algo; // hashing algorithm function
+    size_t bytes_to_print;   // to store number of bytes to print in hash digest
+                             // based on the algorithm
+
+    // open the character device in read/write mode
+    dev = open(DEVICE_PATH, O_RDWR);
+    if (dev < 0)            // error handling
     {
-        perror("Failed to open device"); // error handling
+        perror("Failed to open device");
         return errno;
     }
 
-    char sentence[100];
-    dt dt = generate_datetime();
-    format_sentence(sentence, dt); // sentence for datetime
+    // generate datetime and store to sentence
+    dt = generate_datetime();
+    format_sentence(sentence, dt);
+    // convert to bytes and store in us.plaintext for byte-by-byte accessibility
+    strncpy((char *)us.plaintext, sentence, BUF_SIZE);
 
     // get user input for hashing algorithm
-    int choice;
-    printf("Select a Hashing Algorithm (1-5) from below:\n1. MD5\n2. SHA-512\n3. SHA-382\n4. SHA-256\n5. SHA-1\n\nChoice: ", stdout);
+    printf("Select a Hashing Algorithm (1-5) from below: \
+            \n1. MD5 \
+            \n2. SHA-512 \
+            \n3. SHA-382 \
+            \n4. SHA-256 \
+            \n5. SHA-1 \
+            \n\nChoice: ",
+           stdout);
+    
     scanf("%d", &choice);
+
     if (choice < 0 || choice > 5)
     {
         puts("Please select an option from 1 to 5.");
         return -1;
     }
-
-    userspace_t us;
-    const EVP_MD *hash_algo;
-    char *algoStr = "";
-    strncpy((char *)us.plaintext, sentence, BUF_SIZE); // convert characters of the sentence to bytes and store in user.plaintext array to access data byte by byte if needed
-    // provide hashing algo based on user input
+    
+    // provide hashing algorithm based on user input
     if (choice == 1)
     {
-        us.algo = MD5;         // to store in struct
-        hash_algo = EVP_md5(); // for the algo in OpenSSL
-        algoStr = "MD5";
+        us.algo = MD5;         // to store in us struct
+        algo_str = "MD5";      // type of hashing algorithm
+        hash_algo = EVP_md5(); // hashing algorithm in OpenSSL
+        bytes_to_print = 16;   // MD5 hash digest = 128 bits = 16 bytes
     }
     else if (choice == 2)
     {
         us.algo = SHA512;
+        algo_str = "SHA512";
         hash_algo = EVP_sha512();
-        algoStr = "SHA512";
+        bytes_to_print = 64;   // SHA-512 hash digest = 512 bits = 64 bytes
     }
     else if (choice == 3)
     {
         us.algo = SHA384;
+        algo_str = "SHA384";
         hash_algo = EVP_sha384();
-        algoStr = "SHA384";
+        bytes_to_print = 48;   // SHA-384 hash digest = 384 bits = 48 bytes
     }
     else if (choice == 4)
     {
         us.algo = SHA256;
+        algo_str = "SHA256";
         hash_algo = EVP_sha256();
-        algoStr = "SHA256";
+        bytes_to_print = 32;   // SHA-256 hash digest = 256 bits = 32 bytes
     }
     else if (choice == 5)
     {
         us.algo = SHA1;
+        algo_str = "SHA1";
         hash_algo = EVP_sha1();
-        algoStr = "SHA1";
+        bytes_to_print = 20;   // SHA-1 hash digest = 160 bits = 20 bytes
     }
 
-    // generate hashed_sentence in user space
+    // generate hashed_sentence in userspace
     char hashed_sentence[bytes_to_print];
     hash(sentence, hashed_sentence, hash_algo);
 
-    us.hashtext = hashed_sentence;
-
-    // hashed_sentence from user space
+    // convert hashed_sentence to hex
     char hash_sentence_hex[2 * bytes_to_print + 1];
     for (size_t i = 0; i < bytes_to_print; i++)
-        snprintf(&hash_sentence_hex[2 * i], 3, "%02x", (unsigned char)us.hashtext[i]); // convert digest bytes to hexadecimal
+        snprintf(&hash_sentence_hex[2 * i],
+                 3,
+                 "%02x",
+                 (unsigned char)hashed_sentence[i]);
 
-    // strncpy((char *)us.hashtext, hash_sentence_hex, BUF_SIZE); // convert characters of the sentence to bytes and store in user.plaintext array to access data byte by byte if needed
+    // convert to bytes and store in us.hashtext for byte-by-byte accessibility
+    strncpy((char *)us.hashtext, hash_sentence_hex, BUF_SIZE);
 
     // write to device
     ret = write(dev, &us, sizeof(userspace_t));
@@ -193,35 +197,25 @@ int main(void)
         return errno;
     }
 
-    // calculate number of bytes to print in hash digest based on the algo
-    // MD5 hash digest = 128 bits = 16 bytes
-    // SHA-512 hash digest = 512 bits = 64 bytes
-    // SHA-384 hash digest = 384 bits = 48 bytes
-    // SHA-256 hash digest = 256 bits = 32 bytes
-    // SHA-1 hash digest = 160 bits = 20 bytes
-    size_t bytes_to_print = (us.algo == MD5) ? 16 : (us.algo == SHA512) ? 64
-                                                : (us.algo == SHA384)   ? 48
-                                                : (us.algo == SHA256)   ? 32
-                                                : (us.algo == SHA1)     ? 20
-                                                                        : (puts("Algorithm not supported"), -1);
-
-    // received hashed_sentence from kernel
+    // convert received digest bytes (hash text) from kernel to hexadecimal
     char digest_hex[2 * bytes_to_print + 1];
     for (size_t i = 0; i < bytes_to_print; i++)
-        snprintf(&digest_hex[2 * i], 3, "%02x", (unsigned char)digest[i]); // convert digest bytes to hexadecimal
+        snprintf(&digest_hex[2 * i], 3, "%02x", (unsigned char)digest[i]);
 
-    printf("Original Sentence: %s", us.plaintext);
-    printf("\nHashed Sentence from user space: %s", hash_sentence_hex);
-    printf("\nHashing Algorithm: %s", algoStr);
-    printf("\nHashed Sentence from kernel space: %s", digest_hex);
+    // print output
+    printf("\nOriginal Sentence                 : %s", us.plaintext);
+    printf("\nHashing Algorithm                 : %s", algo_str);
+    printf("\nHashed Sentence from user space   : %s", hash_sentence_hex);
+    printf("\nHashed Sentence from kernel space : %s", digest_hex);
     if (strcmp(hash_sentence_hex, digest_hex) == 0)
     {
-        printf("\nComparison Results: Match");
-    } else
-    {
-        printf("\nComparison Results: Invalid");
+        printf("\nComparison Results                : Match");
     }
-    printf("\n");
+    else
+    {
+        printf("\nComparison Results                : Invalid");
+    }
+    printf("\n\n");
 
     // close the device file
     close(dev);
